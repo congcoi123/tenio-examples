5ee13efc0667a94008cc7789f34ea556f2ae9a50
Kong <tengames.inc@gmail.com>
2025-05-16 08:03:25 +0200

    fix: minor changes

--- src/main/java/com/tenio/examples/example4/handler/PlayerLoggedInHandler.java
+++ src/main/java/com/tenio/examples/example4/handler/PlayerLoggedInHandler.java
@@ -39,6 +39,7 @@ public final class PlayerLoggedInHandler extends AbstractHandler
   @Override
   public void handle(Player player, PlayerLoggedInResult result) {
     if (result == PlayerLoggedInResult.SUCCESS) {
+      player.setNeverDeported(true);
       var parcel =
           map().putZeroArray(SharedEventKey.KEY_ALLOW_TO_ACCESS_UDP_CHANNEL,
               array().addByte(DatagramEstablishedState.ALLOW_TO_ACCESS)
